778956ded5701cb506e69e71471f4abdce82dd56
hanifbg <hanifbg48@gmail.com>
2021-09-24 08:49:27 +0700

    remove unused lib

--- go.mod
+++ go.mod
@@ -3,27 +3,7 @@ module github.com/heroku/go-getting-started
 go 1.13
 
 require (
-	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/Masterminds/vcs v1.13.1 // indirect
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
-	github.com/armon/go-radix v1.0.0 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
-	github.com/gin-gonic/gin v0.0.0-20150626140855-4cc2de6207f4
-	github.com/golang/dep v0.5.4 // indirect
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
-	github.com/jmank88/nuts v0.4.0 // indirect
-	github.com/manucorporat/sse v0.0.0-20150604091100-c142f0f1baea // indirect
-	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
-	github.com/mattn/go-isatty v0.0.0-20150814002629-7fcbc72f853b // indirect
-	github.com/nightlyone/lockfile v0.0.0-20180618180623-0ad87eef1443 // indirect
-	github.com/pelletier/go-toml v1.6.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pushm0v/golddigger v0.0.0-20190330144343-f4bea651d005 // indirect
-	github.com/sdboyer/constext v0.0.0-20170321163424-836a14457353 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
-	gopkg.in/bluesuncorp/validator.v5 v5.9.1 // indirect
-	gopkg.in/yaml.v2 v2.2.7 // indirect
+	github.com/pushm0v/golddigger v0.0.0-20190330144343-f4bea651d005
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
 )
--- main.go
+++ main.go
@@ -1,87 +1,86 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"html/template"
 	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
 	"path"
-	"os"
+
 	"github.com/pushm0v/golddigger"
 )
 
 func handleCount(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
+	if r.Method == "POST" {
 
-        
-        decoder := json.NewDecoder(r.Body)
-        payload := struct {
-            Wealth int 	`json:"wealth"`
-            Name string `json:"line_name"`
-            Gold_amount int `json:"gold_amount"`
-        }{}
-        if err := decoder.Decode(&payload); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+		decoder := json.NewDecoder(r.Body)
+		payload := struct {
+			Wealth      int    `json:"wealth"`
+			Name        string `json:"line_name"`
+			Gold_amount int    `json:"gold_amount"`
+		}{}
+		if err := decoder.Decode(&payload); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-        price, _ := golddigger.DigHargaEmasOrg()
-        nisab := 85 * price
-        Total_wealth := payload.Wealth + (payload.Gold_amount * price)
-        nama := payload.Name
+		price, _ := golddigger.DigHargaEmasOrg()
+		nisab := 85 * price
+		Total_wealth := payload.Wealth + (payload.Gold_amount * price)
+		nama := payload.Name
 
-        if Total_wealth <= nisab {
-        	data := [] struct {
-			        Type int
-			        Name  string
-			        Total_wealth int
-			        Nisab int
-			    } {
-			        { 0, nama, Total_wealth, nisab},
-			    }
-	        jsonInBytes, _ := json.Marshal(data)
-	        w.Header().Set("Content-Type", "application/json")
-	        w.Write(jsonInBytes)
-	        return
-    	} else {
-    		zakat := Total_wealth * 25 / 1000
-        	data := [] struct {
-			        Type int
-			        Name  string
-			        Total_wealth int
-			        Nisab int
-			        Zakat int
-			    } {
-			        { 1, nama, Total_wealth, nisab, zakat},
-			    }
-	        jsonInBytes, _ := json.Marshal(data)
-	        w.Header().Set("Content-Type", "application/json")
-	        w.Write(jsonInBytes)
-        	return
-    	}
-    }
-    http.Error(w, "Only accept POST request", http.StatusBadRequest)
+		if Total_wealth <= nisab {
+			data := []struct {
+				Type         int
+				Name         string
+				Total_wealth int
+				Nisab        int
+			}{
+				{0, nama, Total_wealth, nisab},
+			}
+			jsonInBytes, _ := json.Marshal(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonInBytes)
+			return
+		} else {
+			zakat := Total_wealth * 25 / 1000
+			data := []struct {
+				Type         int
+				Name         string
+				Total_wealth int
+				Nisab        int
+				Zakat        int
+			}{
+				{1, nama, Total_wealth, nisab, zakat},
+			}
+			jsonInBytes, _ := json.Marshal(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonInBytes)
+			return
+		}
+	}
+	http.Error(w, "Only accept POST request", http.StatusBadRequest)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	var filepath = path.Join("views", "index.html")
-    tmpl := template.Must(template.ParseFiles(filepath))
-    if err := tmpl.Execute(w, nil); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	tmpl := template.Must(template.ParseFiles(filepath))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", handleIndex)
-    http.HandleFunc("/save", handleCount)
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/save", handleCount)
 
-    http.Handle("/static/", 
-        http.StripPrefix("/static/", 
-            http.FileServer(http.Dir("assets"))))
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
 
-    fmt.Println("server started at localhost:5000")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    // err := http.ListenAndServe(":5000", nil)
+	fmt.Println("server started at localhost:5000")
+	// err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		panic(err)
 	}
